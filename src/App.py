31870da8a96e662bcb6887d3e1b17ea5c422c1ae
Rolando Sorrentino <rolando4693@gmail.com>
2024-11-26 14:40:29 -0400

    feat: :sparkles: Make Scrollable canvas

--- src/App.py
+++ src/App.py
@@ -38,6 +38,34 @@ class App(customtkinter.CTk):
             font=create_custom_font(font_size=FONT_SIZE_TITLE, weight=FONT_WEIGHT_BOLD),
         )
 
+        # Create a canvas and a frame
+        self.canvas = customtkinter.CTkCanvas(self)
+        self.scrollable_frame = customtkinter.CTkFrame(self.canvas)
+
+        self.scrollbar_vert = customtkinter.CTkScrollbar(
+            master=self, command=self.canvas.yview, orientation="vertical"
+        )
+        self.scrollbar_horiz = customtkinter.CTkScrollbar(
+            master=self, command=self.canvas.xview, orientation="horizontal"
+        )
+
+        self.canvas.configure(yscrollcommand=self.scrollbar_vert.set)
+        self.canvas.configure(xscrollcommand=self.scrollbar_horiz.set)
+
+        self.scrollbar_vert.grid(row=0, column=1, sticky="ns")
+        self.scrollbar_horiz.grid(row=1, column=0, sticky="ew")
+
+        self.canvas.grid(row=0, column=0, sticky="nsew")
+        self.canvas.create_window((0, 0), window=self.scrollable_frame, anchor="nw")
+
+        self.scrollable_frame.bind(
+            "<Configure>",
+            lambda e: self.canvas.configure(scrollregion=self.canvas.bbox("all")),
+        )
+
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(0, weight=1)
+
         # Marco para seleccionar la ubicación
         self.choose_location_composable(graph_manager=graph_manager)
 
@@ -45,18 +73,16 @@ class App(customtkinter.CTk):
         self.visualize_graph_composable(graph_manager=graph_manager)
 
     def on_close(self):
-        # Método para manejar el cierre de la ventana correctamente
         self.quit()
-        self.destroy()  # Destruye la ventana y limpia los recursos
+        self.destroy()
 
     def visualize_graph_composable(
         self,
         graph_manager: "GraphManager",
     ):
         graph_visualizer = GraphVisualizerComposable(
-            master=self, graph_manager=graph_manager
+            master=self.scrollable_frame, graph_manager=graph_manager
         )
-
         graph_visualizer.grid(row=2, column=0, padx=30, pady=30, sticky="new")
 
     def choose_location_composable(
@@ -64,13 +90,19 @@ class App(customtkinter.CTk):
         graph_manager: "GraphManager",
     ):
         choose_locations_frame = ChooseLocationComposable(
-            master=self, graph_manager=graph_manager
+            master=self.scrollable_frame, graph_manager=graph_manager
         )
         choose_locations_frame.grid(row=1, column=0, padx=30, pady=30, sticky="new")
 
     def setup_window(self):
         self.title("Bogotá WalkSync")
-        self.resizable(False, False)
+        self.attributes("-fullscreen", True)  # Start the app in fullscreen mode
+        self.bind(
+            "<Escape>", self.exit_fullscreen
+        )  # Bind the Escape key to exit fullscreen
+
+    def exit_fullscreen(self, event=None):
+        self.attributes("-fullscreen", False)
 
     def run(self):
         self.mainloop()
